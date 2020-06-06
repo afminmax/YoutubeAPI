2abcc7b0a1fa0edfb45524bbb077fc2200e49b5a
afminmax <allen.frasier@aquinix.com>
2020-06-06 21:29:38 +0200

    js robots done

--- classes/js-robots.js
+++ classes/js-robots.js
@@ -6,14 +6,28 @@ class Robot {
     this.color = color;
     this.weight = weight;
   }
+  introduce_self() {
+    console.log(
+      'Domo arrigato I am Mrs Roboto ' +
+        this.name +
+        ', I am ' +
+        this.color +
+        ' and weigh ' +
+        this.weight +
+        ' kilos.'
+    );
+  }
 }
+
+// the 'new' keyword
+// - 1. creates a new empty object
+// - 2. sets the value of 'this' to be the new empty object
+// - 3. calls the constructor method
 
 var r1 = new Robot('Anya', 'Green', 34);
 var r2 = new Robot('Karla', 'Teal', 23);
 console.log(r1);
 console.log(r2);
 
-// the 'new' keyword
-// - 1. creates a new empty object
-// - 2. sets the value of 'this' to be the new empty object
-// - 3. calls the constructor method
+r1.introduce_self();
+r2.introduce_self();
