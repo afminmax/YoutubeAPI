ffd754751ecdef5e27011ffff2e7201f529cce3a
afminmax <allen.frasier@aquinix.com>
2020-06-11 00:16:38 +0200

    added tinydb

--- ytapi/main.py
+++ ytapi/main.py
@@ -6,6 +6,7 @@ from YTchannelStats import ChannelStats
 from pathlib import Path
 from googleapiclient.discovery import build
 import json
+from tinydb import TinyDB, Query
 
 # S2-READING THE API KEY
 dataFolder = Path("X:/Coding/YoutubeAPI/keys/")
@@ -14,24 +15,31 @@ with open(fileToOpen) as file:
     api_key = file.read()
 print('the api key to be used is: ' + api_key)
 
-# S3-GET LIST OF CHANNELS & THEIR YOUTUBE UNIQUE ID's
+# S3 GET LIST OF CHANNELS & THEIR UNIQUE YOUTUBE ID's
 # Currently read from handmade json list to get things started.
 # Later, section 3 will get the list from either MongoDB or TinyDB
-channelList = {}
-with open('channels.json') as json_file:
-    data = json.load(json_file)
-    for channel in data:
-        channelList.update({channel['channelName']: channel['channelId']})
-print(channelList)
+# channelList = {}   # dictionary to hold channel name & id for querying
+# with open('channels.json') as json_file:
+#     data = json.load(json_file)
+#     for channel in data:
+#         channelList.update({channel['channelName']: channel['channelId']})
+# print(channelList)
 
 
-# S4-GET
-for channel in data:
+# build query here:
+# get the ytcId and displayName for each document
+Channel = Query()
+db.search(Channel.ytcId)
+
+# S4-GET THE CURRENT CHANNEL VIDEO AND SUBSCRIBER COUNTS
+for channelName, channelId in channelList.items():
     print('Querying channel ' +
-          channel['channelName'] + ' with channel ID of: ' + channel['channelId'])
-    yt = ChannelStats(api_key, channel['channelId'])
+          channelName + ' with channel ID of: ' + channelId)
+    yt = ChannelStats(api_key, channelId)
     # print(yt)
-    print(yt.get_channel_statistics())
+    channelStatistics = yt.get_channel_statistics()
+    print(channelStatistics)
 
 
+# stop point - install mongodb
 # stop point - read the statistics, compare and update the json file.
