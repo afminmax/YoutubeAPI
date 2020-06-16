9fd1a3518fddd360cbdef98c84ef704e4f183b2e
afminmax <allen.frasier@aquinix.com>
2020-06-16 23:56:38 +0200

    json reads started

--- ytapi/jsonDataModel.py
+++ ytapi/jsonDataModel.py
@@ -7,4 +7,22 @@ import json
 with open('channelsDb.json') as json_file:
     data = json.load(json_file)
 
-data
+print(type(data))
+
+# elements have integeger key id's
+
+# get the id of an element, in this case the first
+data[0]['_id']
+
+# get the youtube id of an element, in this case the first
+data[0]['ytcId']
+
+# get the activity status
+data[0]['channelActive']
+
+# get the channel counts, this has a sub-document aka array
+data[0]['channelCounts']
+
+# get the video count from the channel counts sub-document
+data[0]['channelCounts']
+
