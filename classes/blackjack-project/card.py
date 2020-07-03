a612857f54b03b9c727243f5848033d4e5c33dc3
afminmax <allen.frasier@aquinix.com>
2020-07-03 16:24:45 +0200

    card and deck classes done

--- classes/blackjack-project/card.py
+++ classes/blackjack-project/card.py
@@ -1,53 +1,39 @@
-import random
-from card import Card
-# DECK CLASS OUTLINE
+# CARD CLASS
 
-# Instantiate a new deck
-#   Create all 52 cards of a deck
-#   Hold a list of card objects
+# suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 
-# Shuffle the deck through a method call
-#   Random library, shuffle function
+# # list of ranks
+# ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+#          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
-# Deal cards from the deck
-#   Use the pop method from the deck list, pop pulls out a certain item from a list
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-
-# list of ranks
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-
-# dictionary that maps values to card names
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-          'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+# # dictionary that maps values to card names
+# values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
+#           'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
 
-class Deck:
+class Card():
 
-    # suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 
-    # # list of ranks
-    # ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-    #          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+    # list of ranks
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+             'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
-    # # dictionary that maps values to card names
-    # values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-    #           'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+    # dictionary that maps values to card names
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
+              'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
-    def __init__(self):
-        # deck starts as an empty list
-        self.all_cards = []
-        #
-        for suit in suits:
-            for rank in ranks:
-                created_card = Card(suit, rank)
-                self.all_cards.append(created_card)
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        # this maps the integer value from the class available dictionary
+        self.value = self.values[rank]
 
-    # method to shuffle the cards randomly
-    def shuffle(self):
-        random.shuffle(self.all_cards)
+    def __str__(self):
+        return self.rank + " of " + self.suit
 
-    # method to pull out one card
-    def deal_one(self):
-        return self.all_cards.pop()
+
+# a_card = Card("Hearts", "Three")
+# print(a_card)
+# print(a_card.suit)
+# print(a_card.value)
--- classes/blackjack-project/deck.py
+++ classes/blackjack-project/deck.py
@@ -1,46 +1,24 @@
 import random
 from card import Card
-# DECK CLASS OUTLINE
-
-# Instantiate a new deck
-#   Create all 52 cards of a deck
-#   Hold a list of card objects
-
-# Shuffle the deck through a method call
-#   Random library, shuffle function
-
-# Deal cards from the deck
-#   Use the pop method from the deck list, pop pulls out a certain item from a list
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-
-# list of ranks
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-
-# dictionary that maps values to card names
-values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-          'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
 
 class Deck:
+    suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 
-    # suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+    # list of ranks
+    ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+             'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 
-    # # list of ranks
-    # ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-    #          'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-
-    # # dictionary that maps values to card names
-    # values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
-    #           'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
+    # dictionary that maps values to card names
+    values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
+              'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 
     def __init__(self):
         # deck starts as an empty list
         self.all_cards = []
-        #
-        for suit in suits:
-            for rank in ranks:
+
+        for suit in self.suits:
+            for rank in self.ranks:
                 created_card = Card(suit, rank)
                 self.all_cards.append(created_card)
 
@@ -51,3 +29,17 @@ class Deck:
     # method to pull out one card
     def deal_one(self):
         return self.all_cards.pop()
+
+
+deck = Deck()
+print(deck)
+
+for card in deck.all_cards:
+    print(card)
+
+print('the new deck has: ' + str(len(deck.all_cards)) + ' cards')
+
+deck.shuffle()
+for card in deck.all_cards:
+    print(card)
+print('the shuffled deck has: ' + str(len(deck.all_cards)) + ' cards')
