71bf4da6d01983bcaebf556bf6aa26de48ba849a
afminmax <allen.frasier@aquinix.com>
2020-08-17 16:12:00 +0200

    stat sub-doc to dicts done

--- ytapi/testdata/q_timeSeriesDataQueries.py
+++ ytapi/testdata/q_timeSeriesDataQueries.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import json
 from pathlib import Path
 import datetime
+import pandas as pd
 
 
 # MAKE MONGODB CONNECTION
@@ -17,27 +18,41 @@ print('the db key to be used is: ' + dbc_key)
 print(db)
 print(collection)
 
+# IMPORTANT MONGO FIND NOTE
+# FIND - will give you ALL matching documents to a query
+#        if you have multiple items that may fit a query, use this.
+#        example - similar to the WHERE clause in sql
+
+# FINDONE - will only give you the FIRST matching document to a query.
+#           be sure you really need only one unique item when using.
 
 # ---------------------------------------------------------------------------------
 # 1-LOOK AT ALL ITEMS (SELECT *)
-# outputs all data in json object
+# outputs all data in channel timeseries document
 
 for record in collection.find():
     print(record)
     print('\n')
 
 # ---------------------------------------------------------------------------------
-# 2-LOOK AT THE FIRST ITEM (SELECT (*) TOP 1)
-# outputs all data in json object
-
-print(collection.find_one())
-
-# ---------------------------------------------------------------------------------
-# 3-GET THE VIEWCOUNTS OF A SPECIFIC CHANNEL
-# get the id of one channel, in this case the first which is the zeroth item...
+# 2-GET THE VIEWCOUNTS OF A SPECIFIC CHANNEL
+# get the viewCounts of a channel
+# put in the sub-doc if needed for another query (ex: sub-counts)
 
 # by sub-docs
-collection.find_one({}, {'ytcId': 'abc123', 'viewCount': 1})
+# verbose with ytcid for check
+collection.find_one({'ytcId': 'abc123'}, {'ytcId': 1, 'viewCount': 1})
+# specific for pandas with object id removed
+collection.find_one({'ytcId': 'abc123'}, {'_id': 0, 'viewCount': 1})
+
 
 # by array
-collection.find_one({}, {'ytcId': 'def456', 'viewCount': 1})
+collection.find_one({'ytcId': 'def456'}, {'ytcId': 1, 'viewCount': 1})
+collection.find_one({'ytcId': 'abc123'}, {'_id': 0, 'viewCount': 1})
+
+# ---------------------------------------------------------------------------------
+# 3-PUT THE VIEWCOUNTS INTO A PYTHON DICTIONARY
+# note that the python dictionary get() method extracts only the values
+viewCount_dict = collection.find_one(
+    {'ytcId': 'abc123'}, {'_id': 0, 'viewCount': 1}).get('viewCount')
+print(viewCount_dict)
