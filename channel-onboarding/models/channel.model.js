e3b6e50ab960c1019ff2c865a83b460f8f7ca97a
afminmax <allen.frasier@aquinix.com>
2020-08-21 09:41:42 +0200

    commented out email validation for now

--- channel-onboarding/models/channel.model.js
+++ channel-onboarding/models/channel.model.js
@@ -9,10 +9,10 @@ var channelSchema = new mongoose.Schema({
   email: { type: String },
 });
 
-// Custom validation for email
-channelSchema.path('email').validate((val) => {
-  emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return emailRegex.test(val);
-}, 'Invalid e-mail.');
+//// Custom validation for email
+// channelSchema.path('email').validate((val) => {
+//   emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return emailRegex.test(val);
+// }, 'Invalid e-mail.');
 
 mongoose.model('Channel', channelSchema);
